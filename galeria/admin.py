3b435e67780c716c29edba3dee79ffd09f0b1b6b
Kevin Wutzl <kevin.wutzl8@gmail.com>
2026-02-15 18:26:20 -0300

    curso2

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'publicada')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria', 'publicada')
+    list_per_page = 10
+    list_editable = ('publicada',)
+
+
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import imagem, index
 
 urlpatterns = [
-    path('index.html', index, name='index'),
-    path('imagem.html', imagem, name='imagem'),
+    path('', index, name='index'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
